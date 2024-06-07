082a419df1a94f18de5de0129927f16c3b100944
Someless <150548091+boyoftime@users.noreply.github.com>
2024-06-07 13:51:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function calculateValue() {
-    const piAmount = document.getElementById('piAmount').value;
-    if (piAmount) {
-        const usdValue = (piAmount * 98.78).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-        const tshValue = (piAmount * 256828).toLocaleString('en-US', { style: 'currency', currency: 'TZS' });
-        document.getElementById('results').innerHTML = `USD: ${usdValue}<br>TZS: ${tshValue}`;
-    } else {
-        document.getElementById('results').innerHTML = 'Please enter a valid Pi amount.';
-    }
-}
-
 document.getElementById("rateButton").addEventListener("click", function() {
     document.getElementById("ratePopup").style.display = "block";
 });
@@ -16,3 +5,14 @@ document.getElementById("rateButton").addEventListener("click", function() {
 document.querySelector(".close-button").addEventListener("click", function() {
     document.getElementById("ratePopup").style.display = "none";
 });
+
+function calculateValue() {
+    var piAmount = document.getElementById("piAmount").value;
+    if (piAmount) {
+        var usdValue = (piAmount * 98.78).toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+        var tshValue = (piAmount * 256828).toLocaleString('en-US');
+        document.getElementById("results").innerHTML = `Value in USD: ${usdValue}<br>Value in TSH: ${tshValue}`;
+    } else {
+        document.getElementById("results").innerHTML = "Please enter a Pi amount.";
+    }
+}
